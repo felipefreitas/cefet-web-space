c40b7b9172149d2ab6b92bf9adea818ee0603bb1
Felipe Freitas <felipesafreitas@gmail.com>
2018-04-19 22:14:50 -0300

    added next and previous button to slide

--- exercicio2.js
+++ exercicio2.js
@@ -1,7 +1,7 @@
 // Faça o exercício da galeria de imagens aqui
 // Este arquivo ainda não está incluído no arquivo HTML
 
-const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
+const servidorDasImagens = 'http://fegemo.github.io/cefet-web/images/',
   todasAsImagens = [
     'philae-parts.jpg',
     'philae-rosetta.jpg',
@@ -9,3 +9,31 @@ const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
     'philae-67-picture.jpg',
     'philae-collecting.jpg'
   ];
+
+
+let proximo = document.getElementById('proximo');
+let anterior = document.getElementById('anterior');
+let galeria = document.getElementById('galeria');
+let index = 0;
+
+proximo.addEventListener('click', function(e) {
+	let slide = galeria.getElementsByTagName('img').slide;
+	if (index == todasAsImagens.length -1) {
+		index = 0;
+	} else {
+		index++;
+	}
+	console.log(index + '/' + todasAsImagens.length);
+	slide.src = servidorDasImagens + todasAsImagens[index];
+});
+
+anterior.addEventListener('click', function(e) {
+	let slide = galeria.getElementsByTagName('img').slide;
+	if (index === 0) {
+		index = todasAsImagens.length;
+	} else {
+		index--;
+	}
+	console.log(index + '/' + todasAsImagens.length);
+	slide.src = servidorDasImagens + todasAsImagens[index];
+});
