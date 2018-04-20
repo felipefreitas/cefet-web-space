1545b39e368c1a308ec9986b3a04f673b2ef97c3
Felipe Freitas <felipesafreitas@gmail.com>
2018-04-19 21:04:10 -0300

    added function to expand paragraph text

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,11 @@
 // Faça o exercício dos parágrafos aqui
 // Este arquivo ainda não está incluído no arquivo HTML
+let buttons = document.getElementsByClassName('botao-expandir-retrair');
+
+for (let i = buttons.length - 1; i >= 0; i--) {
+	let button = buttons[i];
+	button.addEventListener('click', function(e){
+		let paragraph = button.parentNode;
+		paragraph.classList.toggle('expandido');
+	});
+}
